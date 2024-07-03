f6f9a0f6960c9ff9d3578d94ba8111f3c08289ff
Fordjour Gyasi Samuel <sgfordjour@st.ug.edu.gh>
2024-07-03 12:39:24 +0000

    Added screens, storage and navigation

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './HomeScreen';
+import CartScreen from './CartScreen';
+import { CartProvider } from './src/context/CartContext';
+
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
-  )
-}
+    <CartProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="" component={HomeScreen} />
+          <Stack.Screen name=" " component={CartScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </CartProvider>
+  );
+};
 
-export default App
-
-const styles = StyleSheet.create({})
+export default App;
