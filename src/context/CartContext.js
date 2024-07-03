9e7dd1494db3e11f2711e8283b44f1e83d121c20
Fordjour Gyasi Samuel <sgfordjour@st.ug.edu.gh>
2024-07-03 12:02:34 +0000

    Initial commit

---
+++ src/context/CartContext.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function CartContext() {
+  return (
+    <View>
+      <Text>CartContext</Text>
+    </View>
+  )
+}
